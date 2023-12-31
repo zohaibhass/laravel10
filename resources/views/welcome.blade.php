0baba9e9fb9eb80f849b35cb0d18cf046a38e76e
zohaib hassan <zohaibhunzai121@gmail.com>
2023-12-31 17:42:48 +0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
                     <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam
                         ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita
                         duo justo magna dolore erat amet</p>
-                    <a href="" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Contact Us</a>
+                    <a href="{{ route('show_availability') }}" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Schedule</a>
                 </div>
                 <div class="col-lg-6 text-center text-lg-end overflow-hidden">
                     <img class="img-fluid" src="img/hero.png" alt="">
